cc6c683aab9609ad92c5fe6ee40b266c9a18b234
GANADALA <leesuyppp@hotmail.com>
2025-06-09 03:34:17 +0900

    필요한 변수들 선언해두기

--- 고양이 수프2/FileName.cpp
+++ 고양이 수프2/FileName.cpp
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+#define ROOM_WIDTH 13
+#define HME_POS 1
+#define BWL_POS (ROOM_WIDTH - 2)
 
 int main(void) {
 
+    int chnmil = 2;     
+    int gibun = 3;             
+    int cp = 0;     //큐트 포인트 cp           
+    int soup = 0;        
+    int pos = HME_POS;     
+    int prevpos = HME_POS; 
+    int cher = 0, scratcher_pos = -1; 
+    int tower = 0, tower_pos = -1;         
+    int toymouse = 0;     
+    int laser = 0;       
+    int turn = 1;          
 
+    char name[20];         
+
+    srand((unsigned int)time(NULL));
 
 
 	return 0;
